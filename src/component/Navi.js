6c5c9f6c843d2f1569f77476066115d83cf5b4a1
4thdraw <4thdraw2@gmail.com>
2023-01-17 11:12:45 +0900

    네비게이션 데이터분리

--- src/component/Navi.js
+++ src/component/Navi.js
@@ -6,6 +6,47 @@ import Scrollspy from "react-scrollspy";
 function Navi(props) {
     const [ naivText, updateText ] = useState(true);
     // json -> map
+    const naviDBfile = {
+         naviDATA : [
+            {
+                d1 : {
+                    naviText : "취준생! 인사드립니다.",
+                    type : "",
+                    href : "/about",
+                    cls :  ["px-3", "font141618"]
+                }
+                
+            },
+            {
+                d1 : {
+                    naviText : "개발성장과정",
+                    type : "a",
+                    href : "#precss",
+                    cls :  ["px-3", "font141618"]
+                }
+                
+            },
+            {
+                d1 : {
+                    naviText : "결과물",
+                    type : "a",
+                    href : "#portfolio",
+                    cls :  ["px-3", "font141618"]
+                }
+                
+            },
+            {
+                d1 : {
+                    naviText : "연락주세요!",
+                    type : "a",
+                    href : "#contact",
+                    cls :  ["px-3", "font141618"]
+                }
+                
+            }
+         ],
+         sns : []
+    }
 
     return (
         <div className={`${navicss.hd}  border-bottom  py-2  fixed-top`}>
@@ -14,22 +55,35 @@ function Navi(props) {
                     <Link to="/">svg</Link>
                 </h1>
                 <Scrollspy id="gnb" className='d-flex'>                
-                    <li>
-                        <Link to="#about" className={`${navicss.me} px-3 font141618`}>취준생! 인사드립니다.</Link>
-                    </li>
-                    <li>
-                        <a href="#precss" className='px-3 font141618'>개발성장과정</a>
-                    </li>
-                    <li>
-                        <a href="#portfolio" className='px-3 font141618'>결과물</a>
-                    </li>
-                    <li>
-                        <a href="#contact" onClick={ () => { updateText(!naivText) }} className='px-3 font141618'>
-                            {
-                                naivText ? '연락주세요!' : 'Contact'                        
-                             }
-                          </a>
-                    </li>
+                    {
+                        naviDBfile.naviDATA.map((item, idx)=>{
+                            const myclass = item.d1.cls.join(" ");
+                        //     if(item.d1.type){
+                        //         return (
+                        //                 <li key={ 'navi'+idx } className={myclass}>
+                        //                 <a href={item.d1.href} >
+                        //                 {item.d1.naviText}
+                        //                 </a>
+                        //                 </li>
+                        //                 )
+                        //    }else{
+                        //         return (
+                        //             <li key={ 'navi'+idx } className={myclass}>
+                        //             <Link to={item.d1.href} >
+                        //             {item.d1.naviText}
+                        //             </Link>
+                        //             </li>
+                        //         )
+                        //    }
+
+                           return (
+                            item.d1.type == !"a" ?
+                            (<li key={'navi'+idx}><Link to={item.d1.href} className={myclass}>{item.d1.naviText}</Link></li>) : 
+                            (<li key={'navi'+idx}><a href={item.d1.href} className={myclass}>{item.d1.naviText}</a></li>)
+                           )
+                            
+                        })
+                    }
                 </Scrollspy>
                 <ul id="sns" className='d-flex'>
                     <li><a href="" target="_blank">깃</a></li>
